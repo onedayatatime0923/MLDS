166e5c29e34b154019c64a10e3cb25a8c9f89f80
kevin <onedayatatime0923@gmail.com>
2018-04-21 09:23:25 +0800

    fj

--- hw2/main.py
+++ hw2/main.py
@@ -5,19 +5,21 @@ assert torch and EncoderRNN and AttnDecoderRNN
 
 EPOCHS=300
 BATCH_SIZE=256
-HIDDEN_LAYER=512
+HIDDEN_LAYER=256
+N_LAYER=3
+DROPOUT=0.2
 
 
 dm = Datamanager()
 dm.get_data('train','./data/training_data/feat','./data/training_label.json','train',batch_size=BATCH_SIZE)
 dm.get_data('test','./data/testing_data/feat','./data/testing_label.json','test',batch_size=BATCH_SIZE,shuffle=False)
 
-encoder=EncoderRNN(4096,HIDDEN_LAYER).cuda()
+encoder=EncoderRNN(4096,HIDDEN_LAYER,N_LAYER,dropout=DROPOUT).cuda()
 torch.save(encoder,'encoder.pt')
-decoder=AttnDecoderRNN(HIDDEN_LAYER,dm.vocab_size).cuda()
+decoder=AttnDecoderRNN(HIDDEN_LAYER,dm.vocab_size,N_LAYER,dropout=DROPOUT).cuda()
 torch.save(decoder,'decoder.pt')
 
-dm.trainIters(encoder, decoder, 'train', 'test', EPOCHS)
+dm.trainIters(encoder, decoder, 'train', 'test', EPOCHS, 'data/output.txt')
 torch.save(encoder,'encoder.pt')
 torch.save(decoder,'decoder.pt')
 
